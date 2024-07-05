068274c292c3a130cdf00f2740447e41ec7d6c0d
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-07-05 15:03:07 +0100

    props added in episodes map function

--- src/TvShowsApp.jsx
+++ src/TvShowsApp.jsx
@@ -2,11 +2,14 @@ import episodes from "./data/gameOfThronesEpisodes.json";
 import EpisodeCard from "./components/EpisodeCard";
 
 export default function TVShowsApp() {
-  const ep1 = episodes[0];
+
+  const episodeCards = episodes.map((episode)=> <EpisodeCard key={episode.id} epTitle={episode.name}
+  season={episode.season} episodeNym={episode.number} episodeMedImg={episode.image.medium} epSum={episode.summary}/>)
+
   return (
     <main className="tvShowsApp">
       <div>
-        <EpisodeCard />
+        {episodeCards}
       </div>
     </main>
   );
