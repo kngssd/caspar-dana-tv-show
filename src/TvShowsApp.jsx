32765a127ee44dfe94c3b732583ff31b5b66d551
2405-007-dk <danakengessova@gmail.com>
2024-07-05 15:52:22 +0100

    add maze.com and episode numbers

--- src/TvShowsApp.jsx
+++ src/TvShowsApp.jsx
@@ -2,15 +2,23 @@ import episodes from "./data/gameOfThronesEpisodes.json";
 import EpisodeCard from "./components/EpisodeCard";
 
 export default function TVShowsApp() {
-
-  const episodeCards = episodes.map((episode)=> <EpisodeCard key={episode.id} epTitle={episode.name}
-  season={episode.season} episodeNum={episode.number} episodeMedImg={episode.image.medium} epSum={episode.summary}/>)
+  const episodeCards = episodes.map((episode) => (
+    <EpisodeCard
+      key={episode.id}
+      epTitle={episode.name}
+      season={episode.season}
+      episodeNum={episode.number}
+      episodeMedImg={episode.image.medium}
+      epSum={episode.summary}
+    />
+  ));
 
   return (
     <main className="tvShowsApp">
-      <div>
-        {episodeCards}
-      </div>
+      <div>{episodeCards}</div>
+      <h4>
+        Materials is taken from <a href="https://TVMaze.com">TVMaze.com</a>
+      </h4>
     </main>
   );
 }
--- src/components/EpisodeCard.jsx
+++ src/components/EpisodeCard.jsx
@@ -1,8 +1,24 @@
 export default function EpisodeCard(props) {
+  function episodeCode(episodeNum, season) {
+    const formattedEpNum = episodeNum.toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    });
+
+    const formattedSeasonNum = season.toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    });
+
+    return `S${formattedSeasonNum}E${formattedEpNum}`;
+  }
+
   return (
     <div className="episode-card">
-      <h1>{props.epTitle}-{props.season + props.episodeNum}</h1>
-      <img src={props.episodeMedImg}/>
+      <h1>
+        {props.epTitle}-{episodeCode(props.episodeNum, props.season)}
+      </h1>
+      <img src={props.episodeMedImg} />
       <p>{props.epSum.slice(3, -4)}</p>
     </div>
   );
