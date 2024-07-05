8de739b69d6025e4352f431d07cd0cd114683848
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-07-05 14:51:21 +0100

    deleted demo files, tidyied file structure

--- src/TvShowsApp.jsx
+++ src/TvShowsApp.jsx
@@ -1,5 +1,5 @@
-import episodes from "../data/gameOfThronesEpisodes.json";
-import EpisodeCard from "../data/EpisodeCard";
+import episodes from "./data/gameOfThronesEpisodes.json";
+import EpisodeCard from "./components/EpisodeCard";
 
 export default function TVShowsApp() {
   const ep1 = episodes[0];
