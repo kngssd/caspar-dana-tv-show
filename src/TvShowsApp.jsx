a84437f91ca0fb7ef9aef3bd631400d280011712
2405-006-CW <2405-006-cw@U-2B5MB7QJAWMTD.corp.amazonworkspaces.com>
2024-07-08 15:15:18 +0100

    search and episode display working

--- src/TvShowsApp.jsx
+++ src/TvShowsApp.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import episodes from "./data/gameOfThronesEpisodes.json";
 import EpisodeCard from "./components/EpisodeCard";
+import isInNameOrSum from "./components/isInNameOrSum";
 
 export default function TVShowsApp() {
-  const episodeCards = episodes.map((episode) => (
+
+  const [episodesToDisplay, setEpisodesToDisplay] =
+    React.useState(episodes);
+
+  function handleSearch(event){
+    setEpisodesToDisplay(episodes.filter((episode) => isInNameOrSum(episode, event.target.value)))
+  }
+
+  const episodeCards = episodesToDisplay.map((episode) => (
     <EpisodeCard
       key={episode.id}
       epTitle={episode.name}
@@ -14,16 +23,13 @@ export default function TVShowsApp() {
     />
   ));
 
-  const [episodesToDisplay, setEpisodesToDisplay] =
-    React.useState(episodeCards);
-
   return (
     <main className="tvShowsApp">
-      <input placeholder="Search" />
+      <input placeholder="Search" onChange={handleSearch} />
       <h3>
         Displaying {episodesToDisplay.length}/{episodes.length} episodes
       </h3>
-      <div>{episodesToDisplay}</div>
+      <div>{episodeCards}</div>
       <h4>
         Materials is taken from <a href="https://TVMaze.com">TVMaze.com</a>
       </h4>
